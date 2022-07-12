40a5d1350511bcb457823902df02293814374c16
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-07-12 17:28:22 -0300

    Modificado el Nav Bar agregando las nuevas imágenes a la vista responsive

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -4,18 +4,28 @@ export default function TopBar() {
     return (
         <div className='topbar'>
             <div className="wrapper">
-                <a href="#intro" className="logo"><img src="/assets/Me.png" alt="" height={60} width={60} /></a>
                 <div className="itemContainer">
-                    <a href="#portfolio" class="navs">Portfolio!</a>
+                    <a href="#intro" className="logo"><img src="/assets/Me.png" alt="" height={60} width={60} /></a>
                 </div>
                 <div className="itemContainer">
-                    <a href="#works" class="navs">Works!</a>
+                    <a href="#portfolio" class="navs"><h4>Portfolio!</h4>
+                        <img src="/assets/portfolio.png" alt="portfolio" className="responsiveImg"/>
+                    </a>
                 </div>
                 <div className="itemContainer">
-                    <a href="#aboutMe" class="navs">About me!</a>
+                    <a href="#works" class="navs"><h4>Works!</h4>
+                        <img src="/assets/works.png" alt="works" className="responsiveImg"/>
+                    </a>
                 </div>
                 <div className="itemContainer">
-                    <a href="#contact" class="navs">Contact Me</a>
+                    <a href="#aboutMe" class="navs"><h4>About me!</h4>
+                        <img src="/assets/aboutme.png" alt="about" className="responsiveImg"/>
+                    </a>
+                </div>
+                <div className="itemContainer">
+                    <a href="#contact" class="navs"><h4>Contact Me!</h4>
+                        <img src="/assets/contactme.png" alt="contact" className="responsiveImg"/>
+                    </a>
                 </div>
             </div>
         </div>
